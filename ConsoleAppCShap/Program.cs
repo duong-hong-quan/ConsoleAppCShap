7b5382a332699ee1131b62aacb41df62c0b9eca8
HONGQUAN <hongquan.contact@gmail.com>
2023-05-17 08:21:46 +0700

    Add project files.

---
+++ ConsoleAppCShap/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleAppCShap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            Console.WriteLine($"Sum = {Add(a,b)}");
+            Console.WriteLine($"Sum ={Sum(100)}");
+            Console.ReadLine();
+        }
+        public static int Add(int x, int y)
+        => x + y;
+
+        /*
+         -Sum
+         */
+
+        public static long Sum(int n)
+        {
+            long total = 0;
+            int i = 1;
+            while (i <= n)
+            {
+                if(i% 2 == 0)
+                {
+                    total += i;
+
+                }
+                i++;
+            }
+            return total;
+        }
+    }
+}
